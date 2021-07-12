3388cba6d6d4635159ed032f850c91ccda92ea2f
Mikhail Menshikov <menshikov@uniweb.ru>
2021-07-12 23:47:53 +0300

    Fix tests for generator

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,36 +1,81 @@
 import { range } from '.';
 
+type CheckParams = {
+  args: Parameters<typeof range>;
+  expected: Array<number>;
+}
+
+const check = ({ args, expected }: CheckParams) => {
+  expect(Array.from(range(...args))).toEqual(expected);
+};
+
 describe("range function", () => {
   test("should support two arguments", () => {
-    expect(range(1, 5)).toEqual([1, 2, 3, 4]);
+    check({
+      args: [1, 5],
+      expected: [1, 2, 3, 4]
+    });
   });
 
   test("should support `step` argument", () => {
-    expect(range(1, 10, 3)).toEqual([1, 4, 7]);
+    check({
+      args: [1, 10, 3],
+      expected: [1, 4, 7]
+    });
   });
 
   test("should work with one argument", () => {
-    expect(range(3)).toEqual([0, 1, 2]);
+    check({
+      args: [3],
+      expected: [0, 1, 2]
+    });
   });
 
   test("should return valid array, when second argument is falsy", () => {
-    expect(range(-5, 0)).toEqual([-5, -4, -3, -2, -1]);
+    check({
+      args: [-5, 0],
+      expected: [-5, -4, -3, -2, -1]
+    });
   });
 
   test("should support negative `step` argument", () => {
-    expect(range(5, 0, -1)).toEqual([5, 4, 3, 2, 1]);
-    expect(range(5, 0, -2)).toEqual([5, 3, 1]);
-    expect(range(6, 0, -2)).toEqual([6, 4, 2]);
-    expect(range(5, 0, -3)).toEqual([5, 2]);
-    expect(range(6, 0, -3)).toEqual([6, 3]);
-    expect(range(7, 0, -3)).toEqual([7, 4, 1]);
+    check({
+      args: [5, 0, -1],
+      expected: [5, 4, 3, 2, 1]
+    });
+    check({
+      args: [5, 0, -2],
+      expected: [5, 3, 1]
+    });
+    check({
+      args: [6, 0, -2],
+      expected: [6, 4, 2]
+    });
+    check({
+      args: [5, 0, -3],
+      expected: [5, 2]
+    });
+    check({
+      args: [6, 0, -3],
+      expected: [6, 3]
+    });
+    check({
+      args: [7, 0, -3],
+      expected: [7, 4, 1]
+    });
   });
 
   test("should return empty array for 0", () => {
-    expect(range(0)).toEqual([]);
+    check({
+      args: [0],
+      expected: []
+    });
   });
 
   test("should return empty array for negative `start`", () => {
-    expect(range(-3)).toEqual([]);
+    check({
+      args: [-3],
+      expected: []
+    });
   });
 });
